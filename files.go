e7da6e0bd06ef574d389139641a8d8fbc6046e54
bruhanda <bruhanda@gmail.com>
2022-02-18 15:44:20 +0200

    write to file func

--- files.go
+++ files.go
@@ -2,16 +2,33 @@ package helpers
 
 import "os"
 
-func WriteToFile(file string, data string) error {
-	fo, err := os.Create(file)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := fo.Close(); err != nil {
-			panic(err)
+func StringToFile(data string, file string) error {
+	data += "\n"
+	var fo *os.File
+	var _, err = os.Stat(file)
+	// create file if not exists
+	if os.IsNotExist(err) {
+		fo, err = os.Create(file)
+		if err != nil {
+			return err
 		}
-	}()
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+	} else {
+		fo, err = os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+	}
+
 	_, err = fo.WriteString(data)
 	if err != nil {
 		return err
